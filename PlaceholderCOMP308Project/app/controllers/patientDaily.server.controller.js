34c71c22391544ba5683131e82ec5ce3f6c1b1e2
Jayce D Lovell <jaycedlovell@gmail.com>
2019-04-16 13:32:10 -0400

    edited controllers and routes for patient Tips

--- PlaceholderCOMP308Project/app/controllers/patientDaily.server.controller.js
+++ PlaceholderCOMP308Project/app/controllers/patientDaily.server.controller.js
@@ -26,6 +26,17 @@ exports.create = function (req, res) {
         }
     });
 };
+exports.read = function (req, res) {
+    res.status(200).json(req.patientDaily);
+};
+exports.patientDailyByID = function (req, res, next, id) {
+    PatientDaily.findById(id).populate('patientId', 'firstName lastName fullName').exec((err, patientDaily) => {if (err) return next(err);
+if (!patientDaily) return next(new Error('Failed to load patientDaily '
+        + id));
+    req.patientDaily = patientDaily;
+    next();
+});
+};
 exports.hasAuthorization = function (req, res, next) {
     if (req.user.isPatient === false) {
         return res.status(403).send({
--- PlaceholderCOMP308Project/app/routes/patientsDaily.server.routes.js
+++ PlaceholderCOMP308Project/app/routes/patientsDaily.server.routes.js
@@ -2,6 +2,9 @@ const users = require('../controllers/users.server.controller');
 const patientsDaily = require('../controllers/patientDaily.server.controller');
 
 module.exports = function (app) {
-    app.route('api/patientsDaily/:patientId')
-        .get(users.requiresLogin, patientsDaily.hasAuthorization, patientsDaily.create);
+    app.route('api/patientsDaily')
+        .post(users.requiresLogin, patientsDaily.hasAuthorization, patientsDaily.create);
+    app.route('/api/patientsDaily/:patientDailyId')
+        .get(patientsDaily.read)
+    app.param('patientDailyId', patientsDaily.patientDailyByID);
 };
